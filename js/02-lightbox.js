21782ee2a00e11d32c8f16662e7efdd9fa99a83e
Marini23 <marina.smile1m@gmail.com>
2023-07-07 00:06:11 +0200

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryList = document.querySelector(`.gallery`);
+const itemsMarkup = creatItemsGalleryMarkup(galleryItems);
+
+
+function creatItemsGalleryMarkup(items) {
+return galleryItems.map(({ preview, description }) => {
+    return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+</li>
+    `;
+    }).join(` `);
+}
+
+galleryList.insertAdjacentHTML(`beforeend`, itemsMarkup);
+// galleryList.addEventListener(`click`, hanlerClickItem);
+// console.log(creatItemsGalleryMarkup(items));
+
+let galleryAll = new SimpleLightbox('.gallery a');
+console.log(galleryAll);
+// (function() {
+//         let gallery = new SimpleLightbox('.gallery a', {captionsData: `alt`, captiondelay: 250,});
+//     })();
