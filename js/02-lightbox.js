ce8b23db8b23895cfe51be06a0557001e0b59fc4
Marini23 <marina.smile1m@gmail.com>
2023-07-07 11:16:24 +0200

    taskAll

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,16 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 const galleryList = document.querySelector(`.gallery`);
 const itemsMarkup = creatItemsGalleryMarkup(galleryItems);
 
 
 function creatItemsGalleryMarkup(items) {
-return galleryItems.map(({ preview, description }) => {
+return galleryItems.map(({ preview, original, description }) => {
     return `
     <li class="gallery__item">
-    <a class="gallery__link" href="large-image.jpg">
+    <a class="gallery__link" href="${original}">
     <img class="gallery__image" src="${preview}" alt="${description}" />
     </a>
 </li>
@@ -19,11 +19,7 @@ return galleryItems.map(({ preview, description }) => {
 }
 
 galleryList.insertAdjacentHTML(`beforeend`, itemsMarkup);
-// galleryList.addEventListener(`click`, hanlerClickItem);
-// console.log(creatItemsGalleryMarkup(items));
-
-let galleryAll = new SimpleLightbox('.gallery a');
-console.log(galleryAll);
-// (function() {
-//         let gallery = new SimpleLightbox('.gallery a', {captionsData: `alt`, captiondelay: 250,});
-//     })();
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
